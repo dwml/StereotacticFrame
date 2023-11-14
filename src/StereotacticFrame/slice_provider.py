330ea9c56aab26f7afa99c935d0ba3b7507383df
D <dirk.loeffen@radboudumc.nl>
2023-11-14 11:43:02 +0100

    Refactor SimpleITK to itk

--- src/StereotacticFrame/slice_provider.py
+++ src/StereotacticFrame/slice_provider.py
@@ -1,18 +1,45 @@
 from pathlib import Path
-import SimpleITK as sitk
+import itk
+
+
+def _reorient_rai(img):
+    orienter = itk.OrientImageFilter.New(img)
+    orienter.UseImageDirectionOn()
+    orienter.SetDesiredCoordinateOrientationToAxial()  # equivalent to RAI
+    orienter.Update()
+    return orienter.GetOutput()
+
+def _extract_slice(itk_img, slice_number: int):
+    extractor_type = itk.ExtractImageFilter[itk.Image[itk.D, 3], itk.Image[itk.D, 2]]
+    extractor = extractor_type.New()
+    extractor.SetInput(itk_img)
+    extractor.SetDirectionCollapseToSubmatrix()
+    inputRegion = itk_img.GetBufferedRegion()
+    size = inputRegion.GetSize()
+    size[2] = 0  # collapse along z
+    start = inputRegion.GetIndex()
+    start[2] = slice_number
+    desiredRegion = inputRegion
+    desiredRegion.SetSize(size)
+    desiredRegion.SetIndex(start)
+    extractor.SetExtractionRegion(desiredRegion)
+    extractor.Update()
+    return extractor.GetOutput()
 
 
 class AxialSliceProvider:
     def __init__(self, image_path: Path):
         self._image_path: Path = image_path
-        self._image: sitk.Image = sitk.DICOMOrient(sitk.ReadImage(self._image_path), "RAI")
+        self._image = itk.imread(self._image_path)
+        self._rai_image = _reorient_rai(self._image)
         self._counter: int = 0
-        self._n_axial_slices: int = self._image.GetSize()[-1]
+        self._n_axial_slices: int = itk.size(self._rai_image)[2]
 
-    def next_slice(self) -> sitk.Image:
-        current_slice = self._image[..., self._counter]
+
+    def next_slice(self):
+        extracted_slice = _extract_slice(self._rai_image, self._counter)
         self._counter += 1
-        return current_slice
+        return extracted_slice
 
     def is_empty(self) -> bool:
         if self._counter >= self._n_axial_slices:
--- tests/test_slice_provider.py
+++ tests/test_slice_provider.py
@@ -1,26 +1,42 @@
 from StereotacticFrame.slice_provider import AxialSliceProvider
-import SimpleITK as sitk
+import itk
 import tempfile
 import pytest
 import numpy as np
 from pathlib import Path
 
+TEST_SHAPE = (250, 275, 300)  # i, j, k in itk is k, j, i in numpy
 
-@pytest.fixture
+
+@pytest.fixture(scope="module")
 def temp_image_path():
     temp_path = Path(tempfile.mkdtemp())
-    temp_array = np.zeros((300, 300, 300))
-    temp_img = sitk.GetImageFromArray(temp_array)
+    temp_array = np.zeros(TEST_SHAPE)
+    temp_img = itk.GetImageFromArray(temp_array)
+    temp_img[0, 25, 50] = 1  # this indexing is numpy indexing, so k, j, i
+    temp_img[249, 75, 100] = 1
+    orienter = itk.OrientImageFilter.New(temp_img)
+    orienter.UseImageDirectionOn()
+    orienter.SetDesiredCoordinateOrientationToSagittal()
+    temp_img = orienter.GetOutput()
     temp_file = temp_path.joinpath("tmp_img.nii.gz")
-    sitk.WriteImage(temp_img, str(temp_file))
+    itk.imwrite(temp_img, str(temp_file))
     yield temp_file
     temp_file.unlink()
     temp_path.rmdir()
 
 
-@pytest.fixture
+# reading the itk image takes long, so I put this in a module scoped fixture
+@pytest.fixture(scope="module")
 def slice_provider(temp_image_path):
     return AxialSliceProvider(temp_image_path)
+
+
+# however we need to reset the counter of the slice provider
+@pytest.fixture(autouse=True)
+def reset_counter(slice_provider):
+    slice_provider._counter = 0
+    return slice_provider
 
 
 def test_slice_provider_initializes_with_image(slice_provider) -> None:
@@ -33,35 +49,49 @@ def test_slice_provider_initializes_fails_without_path() -> None:
 
 
 def test_slice_provider_loads_image_on_initialization(slice_provider, temp_image_path) -> None:
-    img = sitk.ReadImage(temp_image_path)
+    img = itk.imread(temp_image_path)
     assert np.allclose(
-        sitk.GetArrayFromImage(slice_provider._image),
-        sitk.GetArrayFromImage(img))
+        itk.GetArrayFromImage(slice_provider._image),
+        itk.GetArrayFromImage(img))
 
 
-def test_slice_provider_loads_image_in_rai_orientation(slice_provider) -> None:
-    """RAI in this context means left to Right, posterior towards Anterior
-    and superior to Inferior"""
-    orientation = sitk.DICOMOrientImageFilter_GetOrientationFromDirectionCosines(slice_provider._image.GetDirection())
-    expected_direction = "RAI"
-    assert orientation == expected_direction
+def test_slice_provider_rai_image_in_correct_orientation(slice_provider) -> None:
+    """Check if slice_provider correctly reorients to RAI
+
+    RAI in this context means left to Right, posterior towards Anterior
+    and superior to Inferior.
+
+    The fixture provides slice_provider with a sagittal image"""
+    rai_img = slice_provider._rai_image
+    given_direction = rai_img.GetDirection()
+
+    assert np.allclose(
+        np.array(given_direction),
+        np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    )
+
+
+def test_slice_provider_provides_correct_shape(slice_provider) -> None:
+    given_size = slice_provider.next_slice().GetBufferedRegion().GetSize()
+    assert given_size == (TEST_SHAPE[2], TEST_SHAPE[1])
 
 
 def test_slice_provider_provides_superior_slice_first(slice_provider, temp_image_path) -> None:
-    test_img = sitk.DICOMOrient(sitk.ReadImage(temp_image_path), "LPS")
-    # Here I know that the test image is in LPS, so superior is the last slice (-1)
-    assert np.allclose(
-        sitk.GetArrayFromImage(slice_provider.next_slice()),
-        sitk.GetArrayFromImage(test_img[..., -1])
-    )
+    # In the fixture I set the 0, 0 index of the superior slice to 1
+    assert slice_provider.next_slice()[25, 50] == 1.
 
 
 def test_slice_provider_provides_inferior_slice_last(slice_provider, temp_image_path) -> None:
     axial_slice = None
     while not slice_provider.is_empty():
         axial_slice = slice_provider.next_slice()
-    test_img = sitk.DICOMOrient(sitk.ReadImage(temp_image_path), "LPS")
-    assert np.allclose(
-        sitk.GetArrayFromImage(axial_slice),
-        sitk.GetArrayFromImage(test_img[..., 0])
-    )
+
+    assert axial_slice[75, 100] == 1.
+
+
+def test_slice_provider_provides_all_slices(slice_provider) -> None:
+    slice_counter = 0
+    while not slice_provider.is_empty():
+        _ = slice_provider.next_slice()
+        slice_counter += 1
+    assert slice_counter == TEST_SHAPE[0]
