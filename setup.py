68a78dfabf7519bf72daf8411ccccbfdec63e464
dirk <dirk.loeffen@radboudumc.nl>
2023-11-17 11:05:18 +0100

    Package using setuptools

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="StereotacticFrame",
+    version="0.1",
+    description="""A python package to detect a stereotactic frame, used for stereotactic surgery""",
+    url="https://github.com/SFNNijmegen/StereotacticFrame",
+    author="Dirk W.M. Loeffen",
+    author_email="dirk.loeffen@radboudumc.nl",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Issues": "https://github.com/SFNNijmegen/StereotacticFrame/issues"
+    },
+    packages=find_packages(),
+    install_requires=["itk", "numpy"],
+    python_requires=">=3.9",
+
+
+)
