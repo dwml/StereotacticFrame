f718497f6246bdde238772d46013cb96fdc95fb1
dirk <dirk.loeffen@radboudumc.nl>
2025-05-19 08:34:53 +0200

    change blob area

--- stereotacticframe/blob_detection.py
+++ stereotacticframe/blob_detection.py
@@ -13,17 +13,17 @@ def detect_blobs(img_slice: sitk.Image) -> list[tuple[float, float]]:
     blobs_list = []
 
     for label_idx in label_statistics.GetLabels():
-        if not 3 < label_statistics.GetPhysicalSize(label_idx) < 20:  # [mm²]
+        if not 0.5 < label_statistics.GetPhysicalSize(label_idx) < 30.:  # [mm²]
             continue
+        
+        blobs_list.append(label_statistics.GetCenterOfGravity(label_idx))
 
-        if label_statistics.GetWeightedElongation(label_idx) < 1.5:
-            blobs_list.append(label_statistics.GetCenterOfGravity(label_idx))
-            logging.debug(f"Logging physical size: {label_statistics.GetPhysicalSize(label_idx)} for label {label_idx}")
-            logging.debug(f"Logging elongation: {label_statistics.GetElongation(label_idx)} for label {label_idx}")
-            logging.debug(f"Logging principal axes: {label_statistics.GetPrincipalAxes(label_idx)} for label {label_idx}")
-            logging.debug(f"Logging principal moments: {label_statistics.GetPrincipalMoments(label_idx)} for label {label_idx}")
-            logging.debug(f"Logging weighted elongation: {label_statistics.GetWeightedElongation(label_idx)} for label {label_idx}")
-            logging.debug(f"Logging weighted principal axes: {label_statistics.GetWeightedPrincipalAxes(label_idx)} for label {label_idx}")
-            logging.debug(f"Logging weighted principal moments: {label_statistics.GetWeightedPrincipalMoments(label_idx)} for label {label_idx}")
+        logging.debug(f"Logging physical size: {label_statistics.GetPhysicalSize(label_idx)} for label {label_idx}")
+        logging.debug(f"Logging elongation: {label_statistics.GetElongation(label_idx)} for label {label_idx}")
+        logging.debug(f"Logging principal axes: {label_statistics.GetPrincipalAxes(label_idx)} for label {label_idx}")
+        logging.debug(f"Logging principal moments: {label_statistics.GetPrincipalMoments(label_idx)} for label {label_idx}")
+        logging.debug(f"Logging weighted elongation: {label_statistics.GetWeightedElongation(label_idx)} for label {label_idx}")
+        logging.debug(f"Logging weighted principal axes: {label_statistics.GetWeightedPrincipalAxes(label_idx)} for label {label_idx}")
+        logging.debug(f"Logging weighted principal moments: {label_statistics.GetWeightedPrincipalMoments(label_idx)} for label {label_idx}")
 
     return blobs_list
