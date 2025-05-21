aaac7feb23cd65097fe206ef589309bf8613d92c
dirk <dirk.loeffen@radboudumc.nl>
2025-05-21 13:36:06 +0200

    Let slice provider return image and mask

--- stereotacticframe/slice_provider.py
+++ stereotacticframe/slice_provider.py
@@ -8,21 +8,23 @@ def _reorient_rai(img):
 
 
 class Processor(Protocol):
-    def process(self, image: sitk.Image) -> sitk.Image:
-        ...
+    def process(self, image: sitk.Image) -> sitk.Image: ...
 
 
 class AxialSliceProvider:
     def __init__(self, image_path: Path, preprocessor: Processor):
         self._image_path: Path = image_path
-        self._image: sitk.Image = preprocessor.process(sitk.ReadImage(self._image_path))
+        self._image: sitk.Image = sitk.ReadImage(self._image_path)
         self._rai_image: sitk.Image = _reorient_rai(self._image)
+        self._rai_mask: sitk.Image = preprocessor.process(self._rai_image)
         self._counter: int = 0
         self._n_axial_slices: int = self._rai_image.GetSize()[-1]
 
-    def next_slice(self):
+    def next_image_mask_pair(self) -> tuple[sitk.Image, sitk.Image]:
         self._counter += 1
-        return self._rai_image[..., self._counter - 1]
+        return self._rai_image[..., self._counter - 1], self._rai_mask[
+            ..., self._counter - 1
+        ]
 
     def is_empty(self) -> bool:
         if self._counter >= self._n_axial_slices:
