e0409d8f0c3f2a5e2ffeadc1e2655d2303db9d1e
dirk <dirk.loeffen@radboudumc.nl>
2025-05-21 13:33:38 +0200

    Add modality specific threshold on mean

--- stereotacticframe/blob_detection.py
+++ stereotacticframe/blob_detection.py
@@ -1,29 +1,30 @@
 import SimpleITK as sitk
 import logging
 
+
 def _get_label_statistics(label_img: sitk.Image, img: sitk.Image):
     label_statistics = sitk.LabelIntensityStatisticsImageFilter()
     label_statistics.Execute(label_img, img)
     return label_statistics
 
 
-def detect_blobs(img_slice: sitk.Image) -> list[tuple[float, float]]:
-    cc = sitk.ConnectedComponent(img_slice > 0)
+modality_thresholds = {"MR": 65, "CT": 750}
+
+
+def detect_blobs(
+    img_slice: sitk.Image, mask_slice: sitk.Image, modality: str
+) -> list[tuple[float, float]]:
+    cc = sitk.ConnectedComponent(mask_slice > 0)
     label_statistics = _get_label_statistics(cc, img_slice)
     blobs_list = []
 
     for label_idx in label_statistics.GetLabels():
-        if not 0.5 < label_statistics.GetPhysicalSize(label_idx) < 30.:  # [mm²]
+        if not 1 < label_statistics.GetPhysicalSize(label_idx) < 30.0:  # [mm²]
             continue
-        
-        blobs_list.append(label_statistics.GetCenterOfGravity(label_idx))
 
-        logging.debug(f"Logging physical size: {label_statistics.GetPhysicalSize(label_idx)} for label {label_idx}")
-        logging.debug(f"Logging elongation: {label_statistics.GetElongation(label_idx)} for label {label_idx}")
-        logging.debug(f"Logging principal axes: {label_statistics.GetPrincipalAxes(label_idx)} for label {label_idx}")
-        logging.debug(f"Logging principal moments: {label_statistics.GetPrincipalMoments(label_idx)} for label {label_idx}")
-        logging.debug(f"Logging weighted elongation: {label_statistics.GetWeightedElongation(label_idx)} for label {label_idx}")
-        logging.debug(f"Logging weighted principal axes: {label_statistics.GetWeightedPrincipalAxes(label_idx)} for label {label_idx}")
-        logging.debug(f"Logging weighted principal moments: {label_statistics.GetWeightedPrincipalMoments(label_idx)} for label {label_idx}")
+        if not label_statistics.GetMean(label_idx) > modality_thresholds[modality]:
+            continue
+
+        blobs_list.append(label_statistics.GetCenterOfGravity(label_idx))
 
     return blobs_list
